b7c3157ed0429846279f46db20f5527ac035afc2
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-03-25 14:40:24 -0700

    npm i blockchain

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,17 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "dotenv/config";
-import "hardhat-deploy";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'dotenv/config';
+import 'hardhat-deploy';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.18",
+    version: '0.8.18',
     settings: {
       optimizer: {
         enabled: true,
         runs: 1000,
       },
-    }
+    },
   },
 
   namedAccounts: {
@@ -21,7 +21,10 @@ const config: HardhatUserConfig = {
   networks: {
     oktest: {
       url: `https://exchaintestrpc.okex.org`, // RPC URL
-      accounts: process.env.DEPLOYER_PRIVATE_KEY == undefined ? [] : [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+      accounts:
+        process.env.DEPLOYER_PRIVATE_KEY == undefined
+          ? []
+          : [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
       saveDeployments: true,
     },
   },
