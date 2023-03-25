0ce570fcc3a2bbfa06a090447126137375f36a80
Why Oh Why <103012174+dRadiant@users.noreply.github.com>
2023-03-25 10:33:24 -0700

    Deploy on OKC Testnet

--- blockchain/deploy/001_deploy_daotreasury.ts
+++ blockchain/deploy/001_deploy_daotreasury.ts
@@ -1,7 +1,7 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 
-const owners: string[] = [];
+const owners: string[] = ["0x6636CFD2130Db2BE544BbaD987982d38bDDBe448", "0x8b6db0c11d46c118B0137667637f228ab91EA25C", "0x935CC8Efc9E96C5538f7a376342BA633022079A5"];
 const numConfirmationsRequired: number = 2;
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
