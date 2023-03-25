17427b16d8da39d5e442b2de4fb74e499712354c
Why Oh Why <103012174+dRadiant@users.noreply.github.com>
2023-03-25 10:12:07 -0700

    Multisig foundation

--- blockchain/contracts/DAOTreasury.sol
+++ blockchain/contracts/DAOTreasury.sol
@@ -12,6 +12,35 @@ import "./DivisibleNFT.sol";
 // Maybe create a Multisig contract then inherit it in the SimulatedDAOTreasury
 
 contract SimulatedDAOTreasury is IERC721Receiver {
+    event Deposit(address indexed sender, uint amount, uint balance);
+    event SubmitTransaction(
+        address indexed owner,
+        uint indexed txIndex,
+        address indexed to,
+        uint value,
+        bytes data
+    );
+    event ConfirmTransaction(address indexed owner, uint indexed txIndex);
+    event RevokeConfirmation(address indexed owner, uint indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint indexed txIndex);
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint public numConfirmationsRequired;
+
+    struct Transaction {
+        address to;
+        uint value;
+        bytes data;
+        bool executed;
+        uint numConfirmations;
+    }
+
+    // mapping from tx index => owner => bool
+    mapping(uint => mapping(address => bool)) public isConfirmed;
+
+    Transaction[] public transactions;
+    
     function onERC721Received(address, address, uint256, bytes calldata) external pure returns (bytes4) {
         return IERC721Receiver.onERC721Received.selector;
     }
